cfc7b77fd81901b5b4398c6f86c32ae5e2d02299
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-03-23 19:35:08 +0400

    Mint an NFT
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- src/forms/mint-form.tsx
+++ src/forms/mint-form.tsx
@@ -1,25 +1,78 @@
 import { Button, Grid, TextareaAutosize, TextField } from "@mui/material";
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useContext, useState } from "react";
 import LoadingComponent from "../components/loading";
 import { toast } from "react-toastify";
+import { useWallet } from "use-wallet";
+import {
+  getNFTMarketContract,
+  isTransactionMined,
+} from "../utils/contract.utils";
+import { WalletStateContext } from "../utils/WalletStateContext";
+import { SkynetClient, genKeyPairFromSeed } from "skynet-js";
+
+export interface Entry<T> {
+  data: T[];
+  dataKey: string;
+  revision: number;
+}
+
+export interface NftData {
+  tokenId: number;
+  owner: string;
+}
 
 export interface MintFormState {
-  uri: string;
-  price: number;
-  feeAddresses: string;
-  feePercentages: string;
-  royaltyAddresses: string;
-  royaltyPercentages: string;
+  uri?: string;
+  price?: number;
+  feeAddresses?: string;
+  feePercentages?: string;
+  royaltyAddresses?: string;
+  royaltyPercentages?: string;
 }
-const MintForm = () => {
-  const [formState, setFormState] = useState<MintFormState>({
-    uri: "",
-    price: 0,
-    feeAddresses: "",
-    feePercentages: "",
-    royaltyAddresses: "",
-    royaltyPercentages: "",
-  });
+
+const loanContractAddress = process.env
+  .REACT_APP_LOAN_CONTRACT_ADDRESS as string;
+const auctionContractAddress = process.env
+  .REACT_APP_AUCTION_CONTRACT_ADDRESS as string;
+const nftMarketContractAddress = process.env
+  .REACT_APP_NFT_MARKET_CONTRACT_ADDRESS as string;
+const icwContractAddress = process.env.REACT_APP_ICW_CONTRACT_ADDRESS as string;
+
+const onUploadProgress = (progress, { loaded, total }) => {
+  toast(`Saving into DB...${Math.round(progress * 100)}%`);
+};
+
+const client = new SkynetClient("https://siasky.net", {
+  onUploadProgress,
+});
+
+const saveNft = async (nft: NftData) => {
+  try {
+    const { privateKey, publicKey } = genKeyPairFromSeed(nft.owner);
+    const savedNftsKey = nft.owner;
+
+    let entry: Entry<NftData>;
+    try {
+      entry = await client.db.getJSON(publicKey, savedNftsKey);
+    } catch (err) {}
+
+    if (!entry.data) {
+      entry.data = [];
+    }
+
+    entry.data.push(nft);
+    await client.db.setJSON(privateKey, savedNftsKey, entry.data);
+
+    return true;
+  } catch (err) {
+    console.log(err);
+  }
+
+  return false;
+};
+
+const MintForm = () => {  
+  const [formState, setFormState] = useState<MintFormState>({});
   const [showLoading, setShowLoading] = useState(false);
 
   const [isUriError, setIsUriError] = useState(false);
@@ -27,13 +80,61 @@ const MintForm = () => {
   const [isPriceError, setIsPriceError] = useState(false);
   const [priceError, setPriceError] = useState("");
 
-  const handleTextAreaChange= (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-      console.log(event.target);
-    setFormState({
-        ...formState,
-        [event.target.name]: event.target.value,
+  const wallet = useWallet();
+  const walletStateContext = useContext(WalletStateContext);
+
+  const addGnosisChainNetwork = async () => {
+    const chainId = process.env.REACT_APP_CHAIN_ID as string;
+    const chainName = process.env.REACT_APP_CHAIN_NAME as string;
+    const symbol = process.env.REACT_APP_CHAIN_SYMBOL as string;
+    const rpcUrl = process.env.REACT_APP_CHAIN_RPC_URL as string;
+    const explorerUrl = process.env.REACT_APP_CHAIN_EXPLORER_URL as string;
+    try {
+      await wallet.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: chainId }], // Hexadecimal version of 80001, prefixed with 0x
       });
-  }
+    } catch (error: any) {
+      if (error.code === 4902) {
+        try {
+          await wallet.ethereum.request({
+            method: "wallet_addEthereumChain",
+            params: [
+              {
+                chainId: chainId, // Hexadecimal version of 80001, prefixed with 0x
+                chainName: chainName,
+                nativeCurrency: {
+                  name: chainName,
+                  symbol: symbol,
+                  decimals: 18,
+                },
+                rpcUrls: [rpcUrl],
+                blockExplorerUrls: [explorerUrl],
+                iconUrls: [""],
+              },
+            ],
+          });
+        } catch (addError) {
+          console.log("Did not add network");
+        }
+      }
+    }
+  };
+
+  React.useEffect(() => {
+    if (wallet.isConnected && wallet.status === "connected" && wallet.chainId !== +process.env.NEXT_PUBLIC_CHAIN_ID) {
+      addGnosisChainNetwork();
+    }
+  }, [wallet.status]);  
+
+  const handleTextAreaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFormState({
@@ -50,17 +151,13 @@ const MintForm = () => {
   const validateUri = async () => {
     // {"name":"EVERGLOW - Yiren Common Card","description":"Bon Bon Chocolat [THE SHOW April 9th, 2019] It seems like Yiren's beauty can be seen from the universe","image":"https://ipfs.featured.market/ipfs/QmdEkZaTxr3vgCautYqd3KfeKLac9MVRDyGerLiyMsotYu","external_url":"0.04","external_metadata_ipfs":"","attributes":{"thumbnail_url":"https://ipfs.featured.market/ipfs/QmUDjR73nLuVjH6B4MgqPwb3Uzq9uKUzisv1LNYGwGdM6j","content_type":"video/mp4"}}
     try {
-        setShowLoading(true);
-        const response = await (await fetch(formState.uri)).json();
-        setShowLoading(false);
-        if(response.name && response.description && response.image) {
-            return true;
-        }
-    } catch(error) {
-        toast.error("Error validating URI: " + error);
-        setShowLoading(false);
+      const response = await (await fetch(formState.uri)).json();
+      if (response.name && response.description && response.image) {
+        return true;
+      }
+    } catch (error) {
+      toast.error("Error validating URI: " + error);
     }
-    setShowLoading(false);
     return false;
   };
 
@@ -68,57 +165,79 @@ const MintForm = () => {
     const royaltyAddresses = formState.royaltyAddresses.split(",");
     const royaltyPercentages = formState.royaltyPercentages.split(",");
 
-    if(royaltyAddresses.length === 0 || royaltyAddresses[0].length === 0) {
+    if (royaltyAddresses.length === 0 || royaltyAddresses[0].length === 0) {
       toast.error("Royalty address can not be empty");
       return;
     }
 
-    if(royaltyPercentages.length === 0 || royaltyPercentages[0].length === 0) {
+    if (royaltyPercentages.length === 0 || royaltyPercentages[0].length === 0) {
       toast.error("Royalty share percentages can not be empty");
       return;
-    }      
-
-    if(royaltyAddresses.length != royaltyPercentages.length) {
-        toast.error("Royalties and Percentages not the same length - " + royaltyAddresses.length + " : " + royaltyPercentages.length);
-        return false;
     }
 
-    let sum = royaltyPercentages.map(el => +el).reduce((el, sum) => sum += el);
-    if(sum !== 100) {
+    if (royaltyAddresses.length != royaltyPercentages.length) {
+      toast.error(
+        "Royalties and Percentages not the same length - " +
+          royaltyAddresses.length +
+          " : " +
+          royaltyPercentages.length
+      );
+      return false;
+    }
+
+    let sum = royaltyPercentages
+      .map((el) => +el)
+      .reduce((el, sum) => (sum += el));
+    if (sum !== 100) {
       toast.error("Sum of Royalty percentages must equal 100");
       return false;
     }
     return true;
-  }
+  };
 
   const validateFeeAddresses = () => {
-      const feeAddresses = formState.feeAddresses.split(",");
-      const feePercentages = formState.feePercentages.split(",");
+    const feeAddresses = formState.feeAddresses.split(",");
+    const feePercentages = formState.feePercentages.split(",");
 
-      console.log("feeAddresses: ", feeAddresses);
+    if (feeAddresses.length === 0 || feeAddresses[0].length === 0) {
+      toast.error("Owner address can not be empty");
+      return;
+    }
 
-      if(feeAddresses.length === 0 || feeAddresses[0].length === 0) {
-        toast.error("Owner address can not be empty");
-        return;
-      }
+    if (feePercentages.length === 0 || feePercentages[0].length === 0) {
+      toast.error("Owner share percentages can not be empty");
+      return;
+    }
 
-      if(feePercentages.length === 0 || feePercentages[0].length === 0) {
-        toast.error("Owner share percentages can not be empty");
-        return;
-      }      
+    if (feeAddresses.length != feePercentages.length) {
+      toast.error(
+        "Owners and Percentages not the same length - " +
+          feeAddresses.length +
+          " : " +
+          feePercentages.length
+      );
+      return false;
+    }
 
-      if(feeAddresses.length != feePercentages.length) {
-          toast.error("Owners and Percentages not the same length - " + feeAddresses.length + " : " + feePercentages.length);
-          return false;
-      }
+    let sum = feePercentages.map((el) => +el).reduce((el, sum) => (sum += el));
+    if (sum !== 100) {
+      toast.error("Sum of Fee percentages must equal 100");
+      return false;
+    }
+    return true;
+  };
 
-      let sum = feePercentages.map(el => +el).reduce((el, sum) => sum += el);
-      if(sum !== 100) {
-        toast.error("Sum of Fee percentages must equal 100");
-        return false;
-      }
-      return true;
-  }
+  const getMintedTokenIdFromTransactionReceipt = (txReceipt) => {
+    const relevantTransferEvent = txReceipt.events.find(
+      (e) => e.event === "Minted"
+    );
+
+    console.log("relevantTransferEvent: ", relevantTransferEvent.args);
+
+    const tokenId = relevantTransferEvent.args.tokenId.toNumber();
+    const address = relevantTransferEvent.args.to;
+    return { tokenId, address };
+  };
 
   const validate = async () => {
     resetErrors();
@@ -126,33 +245,103 @@ const MintForm = () => {
     if (formState.uri && formState.uri.length <= 0) {
       setUriError("Please enter the token URI");
       setIsUriError(true);
-      return;
+      return false;
     }
 
     if (!(await validateUri())) {
-        setUriError("URI is not a valid NFT json file");
-        setIsUriError(true);        
-        return;
+      setUriError("URI is not a valid NFT json file");
+      setIsUriError(true);
+      return false;
     }
     toast.info("URI is valid");
 
-    if(formState.price < 1) {
-        setPriceError("Price can not be less than one");
-        setIsPriceError(true);
-        return;
+    if (formState.price < 1) {
+      setPriceError("Price can not be less than one");
+      setIsPriceError(true);
+      return false;
     }
 
-    if(!validateFeeAddresses()) {
-        return;
+    if (!validateFeeAddresses()) {
+      return false;
     }
 
-    if(!validateRoyaltyAddresses()) {
-        return;
-    }    
+    if (!validateRoyaltyAddresses()) {
+      return false;
+    }
+
+    return true;
   };
 
-  const mint = () => {
-    validate();
+  const mint = async () => {
+    try {    
+      setShowLoading(true);
+    if (await validate()) {
+      if (wallet.isConnected()) {        
+        const nftMarket = getNFTMarketContract(
+          nftMarketContractAddress,
+          wallet.ethereum
+        );
+        toast("Minting....");
+        const mintTxPromise = nftMarket.mint(
+          formState.uri,
+          formState.price,
+          formState.feeAddresses.split(","),
+          formState.feePercentages.split(","),
+          formState.royaltyAddresses.split(","),
+          formState.royaltyPercentages.split(",")
+        );
+
+        walletStateContext.addNewQueuedTx(
+          mintTxPromise,
+          "Minting NFT Market",
+          {}
+        );
+
+        const mintTx = await mintTxPromise;
+        const mintTxExecuted = await mintTx.wait(1);
+
+        const isMined = await isTransactionMined(
+          wallet.ethereum,
+          mintTxExecuted.transactionHash,
+          +(process.env.REACT_APP_TX_WAIT_BLOCK_COUNT as string)
+        );
+
+        if (!isMined) {
+          toast.error(
+            `Transaction not found after ${
+              process.env.REACT_APP_TX_WAIT_BLOCK_COUNT as string
+            } blocks`
+          );
+        } else {
+          const { tokenId, address } =
+            getMintedTokenIdFromTransactionReceipt(mintTxExecuted);
+
+          if (address !== wallet.account) {
+            throw Error("Account and Wallet Address Minted not the same");
+          }
+
+          toast.success(
+            "NFT Minted Successfully. Check your wallet for the token"
+          );
+
+          toast("Cleaning up...");
+
+          await saveNft({
+            tokenId: tokenId,
+            owner: address,
+          });
+
+          toast(`New Token Minted: ${tokenId}`);
+        }
+      } else {
+        toast.error("Wallet is not connected");
+      }
+    }
+  }catch(error) {
+    toast(`Error occured during minting: ${error}`);
+  } finally {
+    setShowLoading(false);
+  }
   };
 
   return (
@@ -243,7 +432,7 @@ const MintForm = () => {
         </Grid>
         <Grid item xs={12}>
           <TextareaAutosize
-            name="royaltyPercetanges"
+            name="royaltyPercentages"
             aria-label="royalty percentages"
             minRows={3}
             placeholder="Royalty Percentages: the royalty (20%) is shared using these percentages"
---
+++ src/utils/contract.utils.ts
@@ -0,0 +1,56 @@
+import Loans from "../contract-abis/Loans.json";
+import Auctions from "../contract-abis/Auctions.json";
+import NFTMarket from "../contract-abis/NFTMarket.json";
+
+import { ethers } from "ethers";
+
+export const getNFTMarketContract = (address: string, ethereum: any) => {
+  const abi = NFTMarket.abi;
+  return getContract(address, ethereum, abi);
+};
+
+export const getAuctionContract = (address: string, ethereum: any) => {
+  const abi = Auctions.abi;
+  return getContract(address, ethereum, abi);
+};
+
+export const getLoanContract = (address: string, ethereum: any) => {
+  const abi = Loans.abi;
+  return getContract(address, ethereum, abi);
+};
+
+const getContract = (address: string, ethereum: any, abi: any) => {
+  const provider = new ethers.providers.Web3Provider(ethereum); //providers.Web3Provider(web3.currentProvider);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(address, abi, signer);
+  const contractWithSigner = contract.connect(signer);
+  return contractWithSigner;
+};
+
+export const isTransactionMined = async (
+  ethereum: any,
+  hash: string,
+  numberOfBlocks: number
+): Promise<boolean> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      let blockCount = 0;
+      provider.on("block", async (_blockNumber) => {
+        blockCount++;
+        if (blockCount > numberOfBlocks) {
+          provider.removeAllListeners("block");
+          resolve(false);
+        }
+        const txReceipt = await provider.getTransactionReceipt(hash);
+        if (txReceipt && txReceipt.blockNumber) {
+          provider.removeAllListeners("block");
+          resolve(true);
+        }
+      });
+    } catch (error) {
+      console.log(`waiting for transaction rejected with error`, error);
+      reject(error);
+    }
+  });
+};
